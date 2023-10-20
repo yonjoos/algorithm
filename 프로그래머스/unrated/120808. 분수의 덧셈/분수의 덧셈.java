cccde5903cb5f19b23e44fcea2793f7eb55f5f70
yonjoos <121422885+yonjoos@users.noreply.github.com>
2023-10-21 02:04:29 +0900

    [level unrated] Title: 분수의 덧셈, Time: 0.08 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.java
@@ -2,27 +2,17 @@ class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
         int[] answer = {};
         
-        int lcm = -1;
-        for(int i = 1; i <= denom1*denom2 ; i++){
-            if(i % denom1 == 0 && i % denom2 == 0){
-                lcm = i;
-                break;
+        int denom = denom1 * denom2;
+        int numer = numer1*denom2 + numer2*denom1;
+        
+        for(int i = denom; i >=1; i--){
+            if(denom%i == 0 && numer%i == 0){
+                numer /= i;
+                denom /= i;
             }
         }
         
-        int num1 = (lcm/denom1) * numer1;
-        int num2 = (lcm/denom2) * numer2;
-        
-        int num = num1 + num2;
-        
-        int n = 0;
-        for(int i = 1; i <= num && i <= lcm; i++){
-            if(num%i == 0 && lcm%i == 0){
-                n = i;
-            }
-        }
-        
-        answer = new int[]{(num1 + num2)/n, lcm/n};
+        answer = new int[]{numer, denom};
         return answer;
     }
 }
