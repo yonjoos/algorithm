4be94a1011f32150ded39b76b9d5bdcccf05ba4e
yonjoos <121422885+yonjoos@users.noreply.github.com>
2023-10-21 14:56:07 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.13 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -4,12 +4,12 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        String number = Integer.toString(n);
-        int size = number.length();
+        char[] number = Integer.toString(n).toCharArray();
+        int size = number.length;
         
         
         for(int i = 0; i < size; i++){
-            answer += (int)number.charAt(i) - 48;
+            answer += (int)number[i] - 48;
         }
         
         System.out.println(size);
