4b212ab513d698bbf7dfe3b03bd8a5fa98a44bda
yonjoos <121422885+yonjoos@users.noreply.github.com>
2023-10-24 05:04:05 +0900

    [level unrated] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = {};
+        
+        answer = new int[2];
+        for(int i : num_list){
+            if(i%2 == 0){
+                answer[0]++;
+            }
+            else{
+                answer[1]++;
+            }
+        }
+        return answer;
+    }
+}
