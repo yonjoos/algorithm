3e722106af1b517ac31be7f5627e5efc3ff70f35
yonjoos <121422885+yonjoos@users.noreply.github.com>
2023-10-23 16:06:11 +0900

    [level unrated] Title: 나이 출력, Time: 0.02 ms, Memory: 65.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int age) {
+        int answer = 0;
+        
+        int bornIn = 2022 - age + 1;
+        answer = bornIn;
+        return answer;
+    }
+}
