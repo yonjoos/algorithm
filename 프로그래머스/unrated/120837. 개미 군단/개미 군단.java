cd2bd2b50f93cbfdfb5917c1f6301886563d0254
yonjoos <121422885+yonjoos@users.noreply.github.com>
2023-10-26 20:57:53 +0900

    [level unrated] Title: 개미 군단, Time: 0.03 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/unrated/120837. 개미 군단/개미 군단.java
@@ -5,58 +5,27 @@ class Solution {
     public int solution(int hp) {
         int answer = 0;
         
-        
-        
-        int last = hp;
-        for(int i = 0; i < 2; i++){
-            int enemy = hp;
-            int tot = 0;
+
+        int enemy = hp;
+        int tot = 0;
             
-            if(i == 0){
-                int g = enemy / 5;
-                enemy -= g * 5;
-                System.out.println("g : "+g);
             
-                tot += g;
+        int g = enemy / 5;
+        enemy -= g * 5;
+            
+        tot += g;
                 
-                int s = enemy/3;
-                enemy -= s * 3;
-                System.out.println("s : "+s);
+        int s = enemy/3;
+        enemy -= s * 3;
 
-                tot += s;
-            }
-            if(i == 1){
-                
-                int s = enemy/3;
-                enemy -= s * 3;
-                System.out.println("s : "+s);
+        tot += s;
 
-                tot += s;
-                
-                int g = enemy / 5;
-                enemy -= g * 5;
-                System.out.println("g : "+g);
-            
-                tot += g;
-
-            }
             
 
-            int sl = enemy;
+        int sl = enemy;   
+        tot += sl;
             
-            tot += sl;
-            System.out.println("total : "+tot);
-            
-            
-            if(last >= tot){
-                last = tot;
-                System.out.println("last : "+last);
-            }
-            System.out.println();
-            System.out.println();
-        }
-        
-        answer = last;
+        answer = tot;
         
         return answer;
     }
