cb2e695b5fee18b0d4b025b57f09905435db5b4e
yonjoos <121422885+yonjoos@users.noreply.github.com>
2023-11-25 23:20:27 +0900

    [level unrated] Title: 모음 제거, Time: 0.10 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
@@ -3,25 +3,9 @@ import java.util.*;
 class Solution {
     public String solution(String my_string) {
         String answer = "";
+                
+        answer = my_string.replaceAll("[aeiou]", "");
         
-        String[] vowels = new String[]{"a", "e", "i", "o", "u"};
-        
-
-        
-        for(int i = 0; i < 5; i++){
-            String output = "";
-            if(my_string.contains(vowels[i])){
-                String[] con = my_string.split(vowels[i]);
-
-                for(String s : con){
-                    output += s;
-                    System.out.println(output);
-                }
-                my_string = output;
-            }
-        }
-        
-        answer = my_string;
         return answer;
     }
 }
