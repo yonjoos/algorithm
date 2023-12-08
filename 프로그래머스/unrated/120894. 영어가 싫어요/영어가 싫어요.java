4b2a37374579861c1c6872893ed6e6274b733735
yonjoos <joyce977@naver.com>
2023-12-09 02:30:26 +0900

    [level unrated] Title: 영어가 싫어요, Time: 0.09 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/unrated/120894. 영어가 싫어요/영어가 싫어요.java
@@ -4,13 +4,13 @@ class Solution {
 
         String num = new String(numbers);
 
-        String[][] tmp = { {"one","1"}, {"two","2"},  {"three","3"},
-                {"four","4"} , {"five","5"}, {"six", "6"},
-                {"seven","7"}, {"eight","8"}, {"nine","9"}, {"zero", "0"}};
+        String[] tmp = {"zero", "one", "two", "three",
+                        "four", "five", "six", "seven",
+                        "eight", "nine"};
 
         for(int i = 0; i < tmp.length; i++){
-            if(num.contains(tmp[i][0])){
-                num = num.replace(tmp[i][0], tmp[i][1]);
+            if(num.contains(tmp[i])){
+                num = num.replace(tmp[i], String.valueOf(i));
             }
         }
 
