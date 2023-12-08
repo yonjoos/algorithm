6208cf375cf044073646c7c68502ad52786b6b4e
yonjoos <joyce977@naver.com>
2023-12-08 17:58:55 +0900

    [level unrated] Title: 암호 해독, Time: 1.80 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = "";
+        
+        int index = -1 + code;;
+        while(index <= cipher.length()-1){
+            
+            answer += String.valueOf(cipher.charAt(index));
+            index += code;
+        }
+        
+        return answer;
+    }
+}
