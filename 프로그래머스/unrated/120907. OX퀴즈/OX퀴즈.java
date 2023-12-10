38627498049a85af98f4d736e5cd82bd36049e39
yonjoos <joyce977@naver.com>
2023-12-10 14:04:08 +0900

    [level unrated] Title: OX퀴즈, Time: 0.13 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120907. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String[] solution(String[] quiz) {
+        String[] answer = {};
+        answer = new String[quiz.length];
+        int index = 0;
+        
+        int zero = '0';
+        int nine = '9';
+        
+        for(String q : quiz){
+            String[] equation = q.split(" ");
+            int len = equation.length;
+            
+            int sum = Integer.parseInt(equation[0]);
+            int finalSum = Integer.parseInt(equation[len-1]);
+            
+            for(int i = 1; i < len-2; i = i + 2){
+                int num = Integer.parseInt(equation[i+1]);
+                if(equation[i].equals("+")){
+                    sum += num;
+                }
+                else if(equation[i].equals("-")){
+                    sum -= num;
+                }
+            }
+            
+            if(sum == finalSum){
+                answer[index++] = "O";
+            }
+            else answer[index++] = "X";
+        }
+        
+        
+        return answer;
+    }
+}
