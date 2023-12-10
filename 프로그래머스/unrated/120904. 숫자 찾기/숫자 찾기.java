9dd458c753268229ebd9a977f41c0ced6716e04d
yonjoos <joyce977@naver.com>
2023-12-10 13:34:34 +0900

    [level unrated] Title: 숫자 찾기, Time: 0.03 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.java
@@ -5,9 +5,9 @@ class Solution {
         String s = String.valueOf(num);
         String kk = String.valueOf(k);
         
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == kk.charAt(0)) return i + 1;
-        } 
+        if(s.contains(kk)){
+            return s.indexOf(kk) + 1;
+        }
         return -1;
     }
 }
