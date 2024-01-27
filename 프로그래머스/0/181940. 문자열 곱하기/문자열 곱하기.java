f267c13ca5910d30fb1948fbc8c31b899d210682
yonjoos <joyce977@naver.com>
2024-01-27 23:37:29 +0900

    [level 0] Title: 문자열 곱하기, Time: 2.26 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -2,7 +2,10 @@ class Solution {
     public String solution(String my_string, int k) {
         String answer = "";
         
-        answer = my_string.repeat(k);
+        //answer = my_string.repeat(k);
+        for(int i = 0; i < k; i++){
+            answer += my_string;
+        }
         return answer;
     }
 }
